7956d6b44b0f3ea736520555685f70b43d36244e
Radin84a <radinalavi2006@gmail.com>
2025-03-04 14:14:42 +0330

    har har

---
+++ Student.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Radin Alavi Nejad on 04/03/2025.
+//
+
+#include "Student.h"
---
+++ Student.h
@@ -0,0 +1,16 @@
+//
+// Created by Radin Alavi Nejad on 04/03/2025.
+//
+
+#ifndef STUDENT_H
+#define STUDENT_H
+
+
+
+class Student {
+
+};
+
+
+
+#endif //STUDENT_H
